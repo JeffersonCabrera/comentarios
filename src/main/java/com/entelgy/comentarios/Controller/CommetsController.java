3025dcfade588162daa17208463ffc23701fd591
Jefferson Cabrera Villacorta <jeffersoncabreradev@gmail.com>
2021-03-09 01:32:12 -0500

    funcionalidad leer comentarios y Restructurar
    creación de capa controller  y service para las funcionalidades de comentarios y restructuración

--- src/main/java/com/entelgy/comentarios/Controller/CommetsController.java
+++ src/main/java/com/entelgy/comentarios/Controller/CommetsController.java
@@ -1,56 +1,41 @@
-package com.entelgy.comentarios.Controller;
+package com.entelgy.comentarios.controller;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.text.StringEscapeUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.entelgy.comentarios.model.Comment;
 import com.entelgy.comentarios.model.Restructure;
+import com.entelgy.comentarios.service.IRestructureService;
 
 @RestController
 public class CommetsController {
 
-	@Value("${rest.comments}")
-	private String urlRestCommets;
+	@Autowired
+	private IRestructureService restructureService;
 
 	private static final Logger log = LoggerFactory.getLogger(CommetsController.class);
 
 	@PostMapping(value = "/", produces = "application/json;charset=UTF-8")
-	public ResponseEntity<Restructure> restructurar() {
-		RestTemplate restTemple = new RestTemplate();
-
-		ResponseEntity<List<Comment>> response = restTemple.exchange(urlRestCommets, HttpMethod.GET, null,
-				new ParameterizedTypeReference<List<Comment>>() {
-				});
-
-		List<Comment> lsComments = response.getBody();
-		ArrayList<String> ResultadoArray = new ArrayList<String>(); 
+	public ResponseEntity<Restructure> restructurar() throws Exception {
+		List<Comment> lsComments = restructureService.listarComentarios();
+		ArrayList<String> ResultadoArray = new ArrayList<String>();
 
 		for (Comment comment : lsComments) {
-			ResultadoArray.add( comment.getPostId().toString() +"\\"   
-					+ comment.getId().toString()  +"\\" 
-					+  comment.getEmail().toString()  );
+			StringBuilder sb = new StringBuilder();
+			sb.append(comment.getPostId()).append("\\").append(comment.getId()).append("\\").append(comment.getEmail());
+			ResultadoArray.add(sb.toString());
 		}
+
 		Restructure restructure = new Restructure();
 		restructure.setData(ResultadoArray);
-
-		log.info("Hola Mundo \\ dos veces");
-
-		for (String string : ResultadoArray) {
-			log.info("Array restructure:  " + string);
-		}
-
 		return new ResponseEntity<Restructure>(restructure, HttpStatus.OK);
 
 	}
---
+++ src/main/java/com/entelgy/comentarios/service/IRestructureService.java
@@ -0,0 +1,11 @@
+package com.entelgy.comentarios.service;
+
+import java.util.List;
+
+import com.entelgy.comentarios.model.Comment;
+
+public interface IRestructureService {
+
+	List<Comment> listarComentarios() throws Exception;
+
+}
